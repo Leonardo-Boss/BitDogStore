a2252525170d924f36c256f158c1ac7d7f47a4d4
Eduardo V. Argenton <eduardov.argenton@outlook.com>
2024-10-22 16:36:40 -0300

    read_repo + default uf2

---
+++ read_repo.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+def get_apps_configs(DIR: str):
+    apps_configs = []
+    assert "repo.json" in os.listdir(DIR),"repo.json não encontrado"
+    with open(f'{DIR}/repo.json') as file:
+        repo = json.load(file)
+        
+    assert "repository_name" in repo.keys(),"repo.json sem repository_name"
+    assert "apps_folder" in repo.keys(),"repo.json sem apps_folder"
+    for i in repo['apps_folder']:
+        assert os.path.exists(f'{DIR}/{i}'),"diretorio de apps não encontrado"
+        apps = os.listdir(f'{DIR}/{i}')
+        for app in apps:
+            if os.path.exists(f'{DIR}/{i}/{app}/app.json'):
+                with open(f'{DIR}/{i}/{app}/app.json') as file:
+                    config = json.load(file)
+                    config["path"] = f'{DIR}/{i}/{app}'
+                    apps_configs.append(config)
+            config = None
+        apps = None
+    return apps_configs
